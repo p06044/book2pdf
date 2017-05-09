7da29e40757dc267c8c937c411b231aa7e2f337c
sortme tetsuya <sortme@sortme-no-MacBook-Pro.local>
2017-05-09 22:43:00 +0900

    fix

--- book2pdf.sh
+++ book2pdf.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 mkdir moto
 mkdir pdf
-bash /Users/sortme/book2pdf/crop.sh
+bash ~/book2pdf/crop.sh
 wait $!
 mv Scan*.jpg moto
-bash /Users/sortme/book2pdf/rot.sh
+bash ~/book2pdf/rot.sh
 wait $!
-bash /Users/sortme/book2pdf/jpg2pdf.sh
+bash ~/book2pdf/jpg2pdf.sh
 wait $!
 mv rc*.pdf pdf
