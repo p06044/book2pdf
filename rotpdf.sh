f38183f3ba6b94f991857315cea0ee74ca786212
sortme tetsuya <sortme@sortme-no-MacBook-Pro.local>
2017-06-17 13:30:29 +0900

    add new file

--- rotpdf.sh
+++ rotpdf.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 j=1
-k=0 
+k=1 
 for i in *.jpg
 do
 	l=`expr $j % 2`
 	if [ $l == $k ]; then
+		echo ${i}
 		convert -rotate 180 ${i} r${i%.jpg}.pdf
+	else
+		convert ${i} r${i%.jpg}.pdf
 	fi
 	j=`expr $j + 1`
 done
